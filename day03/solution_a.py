f2b832dfc1882d2bc741fccf4d51aabd23eb936a
Jan Jitse <janjitse@gmail.com>
2022-12-11 14:46:26 +0100

    day 03

---
+++ day03/solution_a.py
@@ -0,0 +1,43 @@
+import sys
+
+rucksacks = []
+
+with open(sys.path[0] + "/input.txt", "r") as f:
+    for l in f:
+        rucksack_total = l.strip()
+        item_len = len(rucksack_total) // 2
+        rucksacks.append([rucksack_total[:item_len], rucksack_total[item_len:]])
+
+common_items = []
+for l, r in rucksacks:
+    common = list(set(list(l)).intersection(set(list(r))))
+    common_items += common
+
+total_priority = 0
+
+
+def priority_calc(c) -> int:
+    if c.isupper():
+        priority = (ord(c) - ord("A") + 1) + 26
+    else:
+        priority = ord(c) - ord("a") + 1
+    return priority
+
+
+for c in common_items:
+    total_priority += priority_calc(c)
+
+print(total_priority)
+
+all_badges = []
+for k in range(0, len(rucksacks), 3):
+    rucksack_1 = set(list(rucksacks[k][0] + rucksacks[k][1]))
+    rucksack_2 = set(list(rucksacks[k + 1][0] + rucksacks[k + 1][1]))
+    rucksack_3 = set(list(rucksacks[k + 2][0] + rucksacks[k + 2][1]))
+    badge = list(rucksack_1.intersection(rucksack_2).intersection(rucksack_3))
+    all_badges += badge
+badge_priority = 0
+for b in all_badges:
+    badge_priority += priority_calc(b)
+
+print(badge_priority)
