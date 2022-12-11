56fa7718c019617a7e942e97f34012012a8a40e0
Jan Jitse <janjitse@gmail.com>
2022-12-11 14:46:57 +0100

    day 05

---
+++ day05/solution_a.py
@@ -0,0 +1,38 @@
+import sys
+import re
+
+start_board = []
+moves = []
+with open(sys.path[0] + "/input_xlarge.txt", "r") as f:
+    board = True
+    ordA = ord("A")
+    ordZ = ord("Z")
+    for l in f:
+        if not start_board:
+            start_board = [[] for _ in range(0, len(l), 4)]
+        if len(l.strip()) > 0 and board:
+            if len(l) % 4 != 0:
+                l = l[: len(l) // 4 * 4]
+            for k in range(0, len(l), 4):
+                if ordA <= ord(l[k + 1]) <= ordZ:
+                    start_board[k // 4] += l[k + 1 : k + 2]
+        if len(l.strip()) == 0:
+            board = False
+        if len(l.strip()) > 0 and not board:
+            to_parse = re.findall(r"\d+", l)
+            moves.append([int(t) for t in to_parse])
+
+
+print("Start moving")
+for i, m in enumerate(moves):
+    if i % 1000 == 0:
+        print(f"Moved {i} times")
+    from_pos = m[1] - 1
+    to_pos = m[2] - 1
+    amount = m[0]
+
+    start_board[to_pos] = start_board[from_pos][:amount] + start_board[to_pos]
+    start_board[from_pos] = start_board[from_pos][amount:]
+
+message = "".join([k[0] if k else "_" for k in start_board])
+print(message)
